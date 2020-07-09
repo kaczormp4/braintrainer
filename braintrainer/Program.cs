77cf5d68c19a9b11bc4e98aeee095a3ae6c8729b
Bartek Kaczmarczyk <33695264+kaczormp4@users.noreply.github.com>
2020-07-09 23:05:58 +0200

    dodana funkcja HTP + opis funkcji

--- braintrainer/Program.cs
+++ braintrainer/Program.cs
@@ -10,6 +10,7 @@ namespace braintrainer
         {
             menu();
         }
+        //główna funkcja MENU która zbiera wszystkie funkcje i pozwala użytkownikowi na wybór opcji ktora go interesuje
         static void menu()
         {
             Console.WriteLine("┌---------------------┐");
@@ -17,7 +18,8 @@ namespace braintrainer
             Console.WriteLine("| 1 - CAMPAING        |");
             Console.WriteLine("| 2 - LEVELS          |");
             Console.WriteLine("| 3 - CUSTOM SETTINGS |");
-            Console.WriteLine("| 4 - EXIT GAME       |");
+            Console.WriteLine("| 4 - HOW TO PLAY     |");
+            Console.WriteLine("| 5 - EXIT GAME       |");
             Console.WriteLine("└---------------------┘");
             string mm1;
             mm1 = Console.ReadLine();
@@ -39,10 +41,20 @@ namespace braintrainer
             }
             if (mm1 == "4")
             {
+                howToPlay();
+            }
+            if (mm1 == "5")
+            {
                 Environment.Exit(0);
             }
 
         }
+        // funkcja KAMPANIA odpowiada za tryb kampanii, ktory polega na przechodzeniu poziomow po kolei
+        // tzn jesli przejdziemy level 1 automatycznie odpali się level 2 itd
+        // program nie zapisuje w jakim momęcie skończyliśmy rozgrywkę, dlatego za kazdym razem jak przegramy
+        // musimy zaczynać od nowa (tryb kampanii)
+        // w trybie kampanii jest ustawione 5 poziomów które mozemy przejsć
+        // tryb mozna w bardzo łatwy sposów rozszerzyć dopisując w miejscu gdzie jest //*** funkcje game() 
         static void campaign()
         {
             Console.WriteLine("┌---------------------┐");
@@ -65,9 +77,11 @@ namespace braintrainer
                 game(10, 12, 10);
                 game(15, 20, 15);
                 game(20, 50, 17);
+                //***    <-- (odnośnik do opisu)
                 menu();
             }
         }
+        //funkcja levels odpowiada za wyświetlenie dostępnych poziomów
         static void levels()
         {
             Console.WriteLine("┌------------------------------------┐");
@@ -117,6 +131,8 @@ namespace braintrainer
                 menu();
             }
         }
+        //funkcja CUSTONSETTING pozwala nam na konfigurowanie gry w takie sposób jaki chcemy
+        // mozemy wybrac : ilosć liczb, zakres liczb i czas w jakim będą wyświetlane
         static void customSettings()
         {
             Console.WriteLine("┌----------------------------------------------┐");
@@ -143,6 +159,12 @@ namespace braintrainer
             Console.Clear();
             game(numOfNum, rangeOfNum, viewtime);
         }
+        // funkcja GAME jest najważniejszą funkcją w grze
+        // odpowiada ona za część logiczną gry
+        // została ona napisana w bardzo intuicyjny sposób co pozwala w dalszym etapie na prostą konfigurację gry
+        // do funkcji wprowadzamy ilosc liczb ( zmienna numbers), zakres liczb (zmienna range), czs(zmienna time)
+        // funkcja losuje randomowe liczby i wrzuca je do listy (randomlyNumbers) a następnie wyswietla na ekranie w odpowiednim czasie
+        // jesli gracz poprawnie/niepoprawnie doda wyswietlane liczby program go o tym powiadomi ,
         static void game(int numbers, int range, int time)
         {
             int sumRandom = 0;
@@ -241,6 +263,60 @@ namespace braintrainer
                 menu();
                 // return;
             }
+
+        }
+        // funkcja HOWTOPLAY opisuje gre
+        static void howToPlay()
+        {
+            Console.Clear();
+            Console.WriteLine("┌----------------------------------------------┐");
+            Console.WriteLine("| 0 - BACK TO MAIN MENU                        |");
+            Console.WriteLine("|                                              |");
+            Console.WriteLine("| HOW TO PLAY?                                 |");
+            Console.WriteLine("|                                              |");
+            Console.WriteLine("| Welcome to The author's application / game,  |");
+            Console.WriteLine("| which consists in displaying random numbers  |");
+            Console.WriteLine("| by the komupter, and then adding them        |");
+            Console.WriteLine("| by the player.                               |");
+            Console.WriteLine("|                                              |");
+            Console.WriteLine("| There are 3 modes in the game:               |");
+            Console.WriteLine("| -CAMPAING                                    |");
+            Console.WriteLine("| -LEVELS                                      |");
+            Console.WriteLine("| -CUSTOM SETTINGS                             |");
+            Console.WriteLine("|                                              |");
+            Console.WriteLine("| > Campaign mode is from the                  |");
+            Console.WriteLine("|   smallest to the largest                    |");
+            Console.WriteLine("| > In the LEVELS section we can choose        |");
+            Console.WriteLine("|   the level of difficulty that interests us  |");
+            Console.WriteLine("| > CUSTOM SETTINGS allows us to configure     |");
+            Console.WriteLine("|   the game in any way we are interested in   | ");
+            Console.WriteLine("|   (we can choose the number of numbers,      |");
+            Console.WriteLine("|   range of numbers, time in which            |");
+            Console.WriteLine("|   they will be displayed)                    |");
+            Console.WriteLine("|                                              |");
+            Console.WriteLine("|                                              |");
+            Console.WriteLine("| WARNING!                                     |");
+            Console.WriteLine("| THE NUMBERS CAN REPEAT, so pay attention     |");
+            Console.WriteLine("| to the rectangles displayed during the game. |");
+            Console.WriteLine("| e.g.                                         |");
+            Console.WriteLine("|                                              |");
+            Console.WriteLine("| █     █             █                        |");
+            Console.WriteLine("| █  5       --->     █  5         ---> (5,5)  |");
+            Console.WriteLine("| █                   █     █                  |");
+            Console.WriteLine("| etc.                                         |");
+            Console.WriteLine("|                                              |");
+            Console.WriteLine("| GOOD LUCK!                                   |");
+            Console.WriteLine("|                                              |");
+            Console.WriteLine("| 0 - BACK TO MAIN MENU                        |");
+            Console.WriteLine("└----------------------------------------------┘");
+            string htp;
+            htp = Console.ReadLine();
+            if (htp == "0")
+            {
+                Console.Clear();
+                menu();
+            }
         }
     }
 }
+
